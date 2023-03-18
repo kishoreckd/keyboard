6d1a2acfa18edbdf12904c199ee5acd87efa5f1c
swathiramesh20 <swathisrir@dckap.com>
2023-03-18 13:04:06 +0530

    updated

--- index.js
+++ index.js
@@ -6,28 +6,24 @@ const caps = document.querySelector("#caps");
 const btn = document.querySelectorAll(".btn")
 // console.log(text);
 
+const tops = document.querySelectorAll("#top")
+
+const shift = document.querySelectorAll("#shift")
+
+// adding new array while shift is clicked
+let array = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"]
+
+let array1 = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="]
+
+
 // looping the buttons
 
+// it will affect when the button is clicked
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (e) => {
 
         if (e.target.id === "caps") {
-
-            caps.classList.toggle("red")
-            for (let k = 0; k < buttons.length; k++) {
-                if (caps.classList.contains("red")) {
-                    if (buttons[k].innerText != "caps".toUpperCase && buttons[k].innerText != "backspace" && buttons[k].innerText != "tab" && buttons[k].innerText != "enter"
-                        && buttons[k].innerText != "shift" && buttons[k].innerText != "space" && buttons[k].innerText != ".com") {
-                        buttons[k].innerText = buttons[k].innerText.toUpperCase()
-                    }
-                } else {
-                    if (buttons[k].innerText != "caps" && buttons[k].innerText != "backspace" && buttons[k].innerText != "tab" && buttons[k].innerText != "enter"
-                        && buttons[k].innerText != "shift" && buttons[k].innerText != "space" && buttons[k].innerText != ".com") {
-                        buttons[k].innerText = buttons[k].innerText.toLowerCase()
-                    }
-                }
-            }
-
+            capslock()
         }
         else if (e.target.innerText !== "backspace" && e.target.innerText !== "tab" && e.target.innerText !== "caps" &&
             e.target.innerText !== "enter" && e.target.innerText !== "shift" && e.target.innerText !== ".com"
@@ -37,6 +33,12 @@ for (let i = 0; i < buttons.length; i++) {
 
             // console.log(values);
 
+        }
+
+        else if (e.target.innerText == "shift") {
+
+
+            shiftbutton()
         }
 
 
@@ -57,49 +59,134 @@ for (let i = 0; i < buttons.length; i++) {
 
 
 
+
     })
 }
 
-for (let i = 0; i < btn.length; i++) {
-    // console.log(btn[i]);
-    btn[i].setAttribute('keyname', btn[i].innerText);
-    btn[i].setAttribute('lowerCaseName', btn[i].innerText.toLowerCase());
-}
 
 
 
-window.addEventListener('keydown', function (e) {
 
- 
+// it will affect when the key is pressed
+window.addEventListener('keyup', function (e) {
+    if (e.key.toLowerCase() != "capslock" && e.key.toLowerCase() != "backspace" && e.key.toLowerCase() != "tab" && e.key.toLowerCase() != "enter"
+        && e.key.toLowerCase() != "shift" && e.key.toLowerCase() != "space" && e.key.toLowerCase() != ".com" && e.key.toLowerCase() != "alt" && e.key.toLowerCase() != "control") {
+        text.value += e.key
+    }
+
+    if (e.key.toLowerCase() == "capslock") {
+        capslock()
+    }
+
+
+    if (e.key == "") {
+        text.value += " ";
+    }
+    else if (e.key.toLowerCase() == "shift") {
+    shiftbutton()
+
+    }
+
+
+
+
+
+
+    else if (e.key.toLowerCase() == "backspace") {
+        text.value = text.value.slice(0, text.value.length - 1);
+    }
+
+    else if (e.key.toLowerCase() === "tab") {
+        text.value += "    ";
+    }
+    else if (e.key.toLowerCase() === "alt") {
+        text.value += ".com";
+    }
+    else if (e.key.toLowerCase() === "enter") {
+        text.value += "\n";
+    }
+
+
+
+
     for (let i = 0; i < btn.length; i++) {
-        if (e.key == btn[i].getAttribute('keyname') || e.key == btn[i].getAttribute('lowerCaseName')) {
+        console.log(e.key);
+
+
+        if (e.key.toLowerCase() == btn[i].innerText.toLowerCase()) {
             btn[i].classList.remove('active')
             btn[i].classList.add('remove')
         }
-        
+
+
+
+        // setTimeout(() => {
+        //     btn[i].classList.remove('remove')
+        // }, 200)
+    }
+})
+
+window.addEventListener('keyup', function (e) {
+    for (let i = 0; i < btn.length; i++) {
+
+        if (e.key == btn[i].innerText) {
+            // btn[i].classList.remove('active')
+            btn[i].classList.add('remove')
+
+            // alert("ol")
+        }
+
+
 
         setTimeout(() => {
             btn[i].classList.remove('remove')
         }, 200)
     }
 })
-window.addEventListener('keyup', function (e) {
-    for (let i = 0; i < btn.length; i++) {
-        if (e.key == btn[i].getAttribute('keyname') || e.key == btn[i].getAttribute('lowerCaseName')) {
-            btn[i].classList.remove('active')
-            btn[i].classList.add('remove')
 
-            // alert("ol")
+
+
+function capslock() {
+    caps.classList.toggle("red")
+    for (let k = 0; k < buttons.length; k++) {
+        if (caps.classList.contains("red")) {
+            if (buttons[k].innerText != "caps".toUpperCase && buttons[k].innerText != "backspace" && buttons[k].innerText != "tab" && buttons[k].innerText != "enter"
+                && buttons[k].innerText != "shift" && buttons[k].innerText != "space" && buttons[k].innerText != ".com") {
+                buttons[k].innerText = buttons[k].innerText.toUpperCase()
+            }
+        } else {
+            if (buttons[k].innerText != "caps" && buttons[k].innerText != "backspace" && buttons[k].innerText != "tab" && buttons[k].innerText != "enter"
+                && buttons[k].innerText != "shift" && buttons[k].innerText != "space" && buttons[k].innerText != ".com") {
+                buttons[k].innerText = buttons[k].innerText.toLowerCase()
+            }
         }
-        // if () {
-        //     btn[i].classList.remove('active')
-        //     btn[i].classList.add('remove')
-
-        //     // alert("ol")
-        // }
-
-        setTimeout(() => {
-            btn[i].classList.remove('rem')
-        }, 200)
     }
-})
+}
+
+
+
+function shiftbutton() {
+    for (let i = 0; i < tops.length; i++) {
+        tops[i].classList.toggle("ones")
+
+        if (tops[i].classList.contains("ones")) {
+
+
+
+            for (let i = 0; i < array.length; i++) {
+                tops[i].innerText = array[i]
+                // console.log(array[i]);
+            }
+            // console.log(tops[i]);
+        }
+        else {
+
+
+            for (let i = 0; i < array1.length; i++) {
+                tops[i].innerText = array1[i]
+            }
+
+        }
+
+    }
+}
